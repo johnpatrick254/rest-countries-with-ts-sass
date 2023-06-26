e25de1d5bc1074ede7e636f354b334b56e81a8d0
John Patrick <jpattrick538@gmail.com>
2023-06-26 12:36:45 +0300

    feat:add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+export const Header: React.FC = () => {
+    return <>
+        <nav>
+            <div className="nav-title">
+                <h1>Rest Countries</h1>
+            </div>
+        </nav>
+    </>
+
+}
