bf143c533ac7d24860427f087b3caca11e70e512
John Patrick <jpattrick538@gmail.com>
2023-06-27 17:06:47 +0300

    fix:show pages baded on total items

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,20 +1,28 @@
+import { useEffect, useState } from "react";
+
 interface PaginateProp{
     itemsPerPage:number;
     totalItems:number;
-    handleClick:(pageNum:number)=>void
+    handleClick:(pageNum:number)=>void;
+    currentPage:number;
 }
 
-export const Paginate:React.FC<PaginateProp> = ({itemsPerPage,totalItems,handleClick}) =>{
-    const pageNumbers:number[] =[];
-
-    for(let i = 1; i < Math.ceil(totalItems/itemsPerPage); i++){
-        pageNumbers.push(i)
-    }
+export const Paginate:React.FC<PaginateProp> = ({itemsPerPage,totalItems,handleClick,currentPage}) =>{
+    const [pageNumbers,setPageNumber]=useState<number[]>([]);
+    
+  console.log(totalItems);
+    useEffect(()=>{ 
+        const numbers:number[]=[];
+        for(let i = 1; i < Math.ceil(totalItems/itemsPerPage); i++){
+        numbers.push(i)
+        }
+        setPageNumber(numbers);
+    },[totalItems])
     return <>
     <div className="pagination">
         <ul>
             {
-                pageNumbers.map(num=><li onClick={():void=>handleClick(num)} key={num}>{num}</li>)
+                pageNumbers.map(num=><li className={ num === currentPage ? "currentpage":""} onClick={():void=>handleClick(num)} key={num}>{num}</li>)
             }
         </ul>
     </div>
